55d7972d068728adee6bdbfceac6a2a810f517c9
Ms. Brooks <lbrooks@lwhs.org>
2025-03-17 17:33:17 +0000

    adding firebase

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,28 +1,8 @@
-let myQuestion = "What do you do to lift your mood when you're feeling low?"
- 
-let contents = "";
-
-let Button; 
-
 function setup() {
-  createCanvas(500, 400);
-  Button = createButton("show");
-  Button.position(220,80)
-  Button.mousePressed(showText)
-  
-  background(50);
-  fill(255);
-  textSize(14);
-  text(myQuestion,50,50)  
+  createCanvas(400, 400);
+  background(200);
 }
 
 function draw() {
-  
-}
-function keyTyped() {
-  contents = contents + key;
-}
-
-function showText(){
-   text(contents, 50, 250, 400, 200);
+  // Nothing here yet – we'll add features step by step
 }
